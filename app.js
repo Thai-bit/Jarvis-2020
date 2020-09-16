adcd6f6161812f9564a93eabf2c17eebed832388
Boonthai Yan <56100316+Thai-bit@users.noreply.github.com>
2020-09-16 15:47:25 -0700

    updated package.json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World\n\nCool!');
+    response.end('Hello World. Is it working??\n\nCool!');
 });
 
 server.listen(port, (err) => {
